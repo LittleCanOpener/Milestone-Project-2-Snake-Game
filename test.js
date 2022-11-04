7993627cf15f40ce25ffb5b44e402146e4151afa
LittleCanOpener <hrafn8@gmail.com>
2022-11-04 18:50:07 +0000

    found a fix for the restart

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function main() {
     if (gameOver) {
         if (confirm('You lost. Press ok to restart.')){
-            window.location = '/'
+            window.location = './'
         }
         return
     }
